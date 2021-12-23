84513dabf7c01594480e33ffa66642972402aab3
SeungsooChoi <tmdtn5389@gmail.com>
2021-12-23 17:52:12 +0900

    feat : mutation test

--- index.js
+++ index.js
@@ -1,25 +1,86 @@
 import { ApolloServer, gql } from "apollo-server";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+/**
+ * sigunguName : 시군구명
+ * sigunguCode : 시군구코드
+ * name : 시설명
+ * inOutDoorDivName : 실내외구분명
+ * manageMainName : 관리주체명
+ * contactNo : 연락처
+ * homepageAddr : 홈페이지 주소
+ * divingLength : 다이빙장 길이(m)
+ * divingWidth : 다이빙장 폭(m)
+ * divingDepth : 다이빙장 깊이
+ * regPoolLength : 정규 경영장 길이(m)
+ * regPoolWidth : 정규 경영장 폭(m)
+ * regPoolLaneCnt : 정규 경영장 레인수
+ * irregPoolLength : 비정규 경영장 길이(m)
+ * irregPoolWidth : 비정규 경영장 폭(m)
+ * irregPoolLaneCnt : 비정규 경영장 레인수
+ * seatCnt : 관람석 좌석수
+ * personCnt : 수용인원수(명)
+ * latitude : 위도
+ * longitude : 경도
+ * remarks : 비고사항
+ */
 const typeDefs = gql`
+  type Pool {
+    sigunguName: String
+    sigunguCode: String
+    name: String
+    inOutDoorDivName: String
+    manageMainName: String
+    contactNo: String
+    homepageAddr: String
+    divingLength: Int
+    divingWidth: Int
+    divingDepth: Int
+    regPoolLength: Int
+    regPoolWidth: Int
+    regPoolLaneCnt: Int
+    irregPoolLength: Int
+    irregPoolWidth: Int
+    irregPoolLaneCnt: Int
+    seatCnt: Int
+    personCnt: Int
+    latitude: String
+    longitude: String
+    remarks: String
+  }
+
   type Query {
-    hello: String
+    pools: [Pool]
+    # pool: Pool
+  }
+
+  type Mutation {
+    createPool(name: String!): Boolean
+    deletePool(name: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "hi",
+    pools: () => [],
+    // pool: () => ({ name }),
+  },
+  Mutation: {
+    createPool: (_, { name }) => {
+      console.log(name);
+      return true;
+    },
+    deletePool: (_, { name }) => {
+      console.log(name);
+      return true;
+    },
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
+/**
+ * return : url, server instance
+ */
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
